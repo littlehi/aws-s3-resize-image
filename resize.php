20672e3383230825046dd03241bf542b02da644f
Gao <gaoyj@c4b301d071a9.ant.amazon.com>
2017-08-30 12:25:39 +0800

    new projcet

---
+++ resize.php
@@ -0,0 +1,55 @@
+<?php
+date_default_timezone_set('Asia/shanghai');
+require 'vendor/autoload.php';
+
+use Aws\S3\S3Client;
+use Aws\Exception\AwsException;
+
+$bucketName='images.littlehi.com';
+
+$notFoundImg='404.jpg';
+
+$src=$_GET['src'];
+
+if(preg_match("/^(.*)_(\d+)x(\d+).([a-z]+)$/",$src,$matches)){
+
+$objectName=$matches[1].'.'.$matches[4];
+$width=$matches[2];
+$height=$matches[3];
+
+
+$client = new S3Client([ 'region' => 'cn-north-1', 'version' => 'latest']);
+$client->registerStreamWrapper();
+
+try{
+  $imagick = new \Imagick('s3://'.$bucketName.'/'.$objectName);
+}
+catch (Exception $e) {
+  $imagick= new \Imagick($notFoundImg);
+}
+
+if($imagick){
+  $contentType=$imagick->getImageMimeType();
+
+  $imagick->scaleImage($width,$height);
+
+  header("Content-type: ".$contentType);
+  echo $imagick->getImageBlob();
+
+  $client->putObject([
+    'ContentType' => '$contentType',
+    'Body' =>$imagick->getImageBlob(),
+    'Bucket' => $bucketName,
+    'Key' => $src,
+    'StorageClass' => 'REDUCED_REDUNDANCY',
+    'Tagging' => 'thumbnail=true'
+  ]);
+  die();
+}
+
+
+
+}
+
+header("Content-type: image/jpeg");
+echo file_get_contents($notFoundImg);
